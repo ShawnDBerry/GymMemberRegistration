16296b00575728d6cc69d9bafcf87fd1616ee155
User <user@Users-Macintosh.local>
2019-12-14 18:06:49 -0500

    commiting during build

---
+++ app/src/main/java/com/example/gymmemberregistration/model/GymMember.java
@@ -0,0 +1,71 @@
+package com.example.gymmemberregistration.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class GymMember implements Parcelable {
+    private String gymMemberId;
+    private String gymMemberName;
+
+    protected GymMember(Parcel in) {
+        gymMemberId = in.readString();
+        gymMemberName = in.readString();
+        gymMemberPlan = in.readString();
+    }
+
+    public static final Creator<GymMember> CREATOR = new Creator<GymMember>() {
+        @Override
+        public GymMember createFromParcel(Parcel in) {
+            return new GymMember(in);
+        }
+
+        @Override
+        public GymMember[] newArray(int size) {
+            return new GymMember[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(gymMemberId);
+        dest.writeString(gymMemberName);
+        dest.writeString(gymMemberPlan);
+    }
+
+    private String gymMemberPlan;
+
+    public GymMember(String gymMemberId, String gymMbemberName, String gymMemberPlan) {
+        this.gymMemberId = gymMemberId;
+        this.gymMemberName = gymMbemberName;
+        this.gymMemberPlan = gymMemberPlan;
+    }
+
+    public String getGymMemberId() {
+        return gymMemberId;
+    }
+
+    public void setGymMemberId(String gymMemberId) {
+        this.gymMemberId = gymMemberId;
+    }
+
+    public String getGymMbemberName() {
+        return gymMemberName;
+    }
+
+    public void setGymMbemberName(String gymMbemberName) {
+        this.gymMemberName = gymMbemberName;
+    }
+
+    public String getGymMemberPlan() {
+        return gymMemberPlan;
+    }
+
+    public void setGymMemberPlan(String gymMemberPlan) {
+        this.gymMemberPlan = gymMemberPlan;
+    }
+}
